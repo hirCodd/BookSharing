5010680c71bc04f7a3b2deca6f3142f9ec4f4488
hirCodd <hefengen@hotmail.com>
2019-02-13 03:37:44 +0800

    update user's pages

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -15,21 +15,25 @@
             title="我发布的"
             value="20条"
             is-link
+            @click="onClickPublish"
           />
           <van-cell
             title="我卖出的"
             value="20条"
             is-link
+            @click="onClickSell"
           />
           <van-cell
             title="我买到的"
             value="20条"
             is-link
+            @click="onClickGet"
           />
           <van-cell
             title="我的心愿书单"
             value="20条"
             is-link
+            @click="onClickWish"
           />
           </van-cell-group>
       </div>
@@ -40,16 +44,56 @@
           <van-cell
             title="设置"
             is-link
+            @click="onClickSettings"
           />
           <van-cell
             title="帮助与反馈"
             is-link
+            @click="onClickFaq"
           />
           </van-cell-group>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onClickPublish () {
+      wx.navigateTo({
+        url: '/pages/user-publish/main'
+      })
+    },
+    onClickSell () {
+      wx.navigateTo({
+        url: '/pages/user-selling/main'
+      })
+    },
+    onClickGet () {
+      wx.navigateTo({
+        url: '/pages/user-get/main'
+      })
+    },
+    onClickWish () {
+      wx.navigateTo({
+        url: '/pages/user-wish-booklist/main'
+      })
+    },
+    onClickSettings () {
+      wx.navigateTo({
+        url: '/pages/user-settings/main'
+      })
+    },
+    onClickFaq () {
+      wx.navigateTo({
+        url: '/pages/user-faq/main'
+      })
+    }
+  }
+}
+</script>
+
 
 <style scoped>
 .selfinfo {
---
+++ src/pages/user-settings/index.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container">
+    <!-- 用于显示搜索 -->
+    <div class="top-search">
+      <van-search
+        placeholder="请输入搜索关键词"
+        use-action-slot
+        bind:search="onSearch"
+      >
+        <view slot="action" bind:tap="onSearch">搜索</view>
+      </van-search>
+    </div>
+    <!-- 列表 -->
+    <div class="list-content">
+      <van-panel use-footer-slot>
+        <view slot="header">
+          <van-card
+          num="2"
+          price="10.00"
+          desc="描述信息"
+          title="标题"
+          thumb="https://i.loli.net/2017/08/21/599a521472424.jpg"
+          >
+          </van-card>
+        </view>
+         <view slot="footer">
+          <van-row>
+          <van-col span="12">
+            点赞:
+            <van-icon name="like-o" />
+          </van-col>
+          <van-col span="12">
+            666人想要
+          </van-col>
+        </van-row>
+        </view>
+      </van-panel>
+
+      <van-panel use-footer-slot>
+        <view slot="header">
+          <van-card
+          num="2"
+          price="10.00"
+          desc="描述信息"
+          title="标题"
+          thumb="https://i.loli.net/2017/08/21/599a521472424.jpg"
+          >
+          </van-card>
+        </view>
+         <view slot="footer">
+          <van-row>
+          <van-col span="12">
+            点赞:
+            <van-icon name="like-o" />
+          </van-col>
+          <van-col span="12">
+            666人想要
+          </van-col>
+        </van-row>
+        </view>
+      </van-panel>
+
+      <van-panel use-footer-slot>
+        <view slot="header">
+          <van-card
+          num="2"
+          price="10.00"
+          desc="描述信息"
+          title="标题"
+          thumb="https://i.loli.net/2017/08/21/599a521472424.jpg"
+          >
+          </van-card>
+        </view>
+         <view slot="footer">
+          <van-row>
+          <van-col span="12">
+            加入心愿书单
+            <van-icon name="like-o" />
+          </van-col>
+          <van-col span="12">
+            666人想要
+          </van-col>
+        </van-row>
+        </view>
+      </van-panel>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.top-search {
+  width: 95%;
+}
+.list-content {
+  width: 90%;
+  margin-top:5px;
+}
+
+.card-list {
+  margin-top: 5px;
+}
+</style>
