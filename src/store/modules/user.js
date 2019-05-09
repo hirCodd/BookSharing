12414724f482233f5aaede731d47ef8736b3bbbe
HiCodd <hefengen@hotmail.com>
2019-05-09 17:17:42 +0800

    Update user.js

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,13 +1,15 @@
 const state = {
   _status: false,
   isLogin: false,
-  _userInfo: {}
+  _userInfo: {}, // 存储微信用户获取到的信息
+  userData: {}  // 存储后端返回的用户信息
 }
 
 const getters = {
   status: state => state._status,
   userInfo: state => state._userInfo,
-  isLogin: state => state.isLogin
+  isLogin: state => state.isLogin,
+  userData: state => state.userData
 }
 
 const mutations = {
@@ -19,6 +21,10 @@ const mutations = {
   },
   changeLoginStatus (state, status) {
     state.isLogin = status
+  },
+  // params为用户后端的数据，主要是用来返回user_id
+  changeUserData (state, params) {
+    state.userData = params
   }
 }
 
